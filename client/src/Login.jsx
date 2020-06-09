a57ab5735066273d1e888b98008c35af4028c7ed
AndyLQ <Andy_le_quach@hotmail.com>
2020-06-09 15:31:04 +0200

    first login test, frontend test

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -3,7 +3,18 @@ import React, { Component } from "react";
 class Login extends Component {
   state = {};
   render() {
-    return <h2 className="center">This is the login screen</h2>;
+    return (
+      <div className="container">
+        <h3>Login to unlock all features</h3>
+        <form action="">
+          <label htmlFor="">Username</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="text" />
+          <button className="btn yellow darken-2">Login</button>
+        </form>
+      </div>
+    );
   }
 }
 
